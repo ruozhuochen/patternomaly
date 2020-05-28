e3c3c66c3ace9265ce5b66299bec13867959b7e9
gor <ruozhuo.chen@gmail.com>
2020-05-28 15:25:19 +1000

    update index.d.ts
    update the export statements

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,12 @@
-/**
- * draw: Generate a single (HTML5) canvas pattern
- * @param shapeType pattern shape type
- * @param backgroundColor background color of pattern e.g. '#1f77b4'
- * @param patternColor color of the pattern e.g. '#000000'
- * @param size of pattern "gutter"
- */
-
-/**
- * generate: Generates an array of random patterns with the given background colors
- */
-
-declare module "patternomaly/draw" {
+declare module "patternomaly" {
+    
+    /**
+     * draw: Generate a single (HTML5) canvas pattern
+     * @param shapeType pattern shape type
+     * @param backgroundColor background color of pattern e.g. '#1f77b4'
+     * @param patternColor color of the pattern e.g. '#000000'
+     * @param size of pattern "gutter"
+     */
     export function draw(
     shapeType: 'plus' | 'cross' | 'dash' | 'cross-dash' | 'dot' | 'dot-dash' | 'disc' | 'ring' | 'line' | 'line-vertical' | 'weave' | 'zigzag' | 'zigzag-vertical' | 'diagonal' | 'diagonal-right-left' | 'square' | 'box' | 'triangle' | 'triangle-inverted' | 'diamond' | 'diamond-box',
     /** background color of pattern e.g. '#1f77b4' */
@@ -18,8 +14,9 @@ declare module "patternomaly/draw" {
     /** color of the pattern e.g. #000000 */
     patternColor?: string,
     size?: number): CanvasPattern;
-}
-
-declare module "patternomaly/generate" {
+    
+    /**
+     * generate: Generates an array of random patterns with the given background colors
+     */
     export function generate(colorList: Array<string>): Array<CanvasPattern>;
 }
