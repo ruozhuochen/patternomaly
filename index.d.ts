ae71f31c3d87680d4dea5f0382ab67922a0b35ba
gor <ruozhuo.chen@gmail.com>
2020-05-28 14:57:17 +1000

    update export functions

--- index.d.ts
+++ index.d.ts
@@ -6,7 +6,8 @@
  * @param size of pattern "gutter"
  */
 
-declare module "patternomaly/draw" {
+declare module "patternomaly" {
+    const generate: (colorList: Array<string>) => Array<CanvasPattern>;
     const draw:
     (shapeType: 'plus' | 'cross' | 'dash' | 'cross-dash' | 'dot' | 'dot-dash' | 'disc' | 'ring' | 'line' | 'line-vertical' | 'weave' | 'zigzag' | 'zigzag-vertical' | 'diagonal' | 'diagonal-right-left' | 'square' | 'box' | 'triangle' | 'triangle-inverted' | 'diamond' | 'diamond-box',
     /** background color of pattern e.g. '#1f77b4' */
@@ -15,8 +16,5 @@ declare module "patternomaly/draw" {
     patternColor?: string,
     size?: number) => CanvasPattern;
     export = draw;
-}
-declare module "patternomaly/generate" {
-    const generate: (colorList: Array<string>) => Array<CanvasPattern>;
     export = generate;
 }
