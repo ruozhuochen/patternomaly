2c5767ac4d530a40540895f4715b1c2824c6fbad
gor <ruozhuo.chen@gmail.com>
2020-05-28 16:36:08 +1000

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -5,12 +5,10 @@
  * @param patternColor color of the pattern e.g. '#000000'
  * @param size of pattern "gutter"
  */
-declare function draw(
+export declare function draw(
     shapeType: 'plus' | 'cross' | 'dash' | 'cross-dash' | 'dot' | 'dot-dash' | 'disc' | 'ring' | 'line' | 'line-vertical' | 'weave' | 'zigzag' | 'zigzag-vertical' | 'diagonal' | 'diagonal-right-left' | 'square' | 'box' | 'triangle' | 'triangle-inverted' | 'diamond' | 'diamond-box',
     /** background color of pattern e.g. '#1f77b4' */
     backgroundColor: string,
     /** color of the pattern e.g. #000000 */
     patternColor?: string,
     size?: number): CanvasPattern
-
-export = draw;
