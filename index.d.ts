9ef670ec2ab7dc7472d72176721cdb895069b8f8
gor <ruozhuo.chen@gmail.com>
2020-05-28 15:19:54 +1000

    update index.d.ts
    update the definition again

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,25 @@
 /**
- * Generate a single (HTML5) canvas pattern
+ * draw: Generate a single (HTML5) canvas pattern
  * @param shapeType pattern shape type
  * @param backgroundColor background color of pattern e.g. '#1f77b4'
  * @param patternColor color of the pattern e.g. '#000000'
  * @param size of pattern "gutter"
  */
 
-declare module "patternomaly" {
-    function generate(colorList: Array<string>): Array<CanvasPattern>;
-    function draw(shapeType: 'plus' | 'cross' | 'dash' | 'cross-dash' | 'dot' | 'dot-dash' | 'disc' | 'ring' | 'line' | 'line-vertical' | 'weave' | 'zigzag' | 'zigzag-vertical' | 'diagonal' | 'diagonal-right-left' | 'square' | 'box' | 'triangle' | 'triangle-inverted' | 'diamond' | 'diamond-box',
+/**
+ * generate: Generates an array of random patterns with the given background colors
+ */
+
+declare module "patternomaly/draw" {
+    export function draw(
+    shapeType: 'plus' | 'cross' | 'dash' | 'cross-dash' | 'dot' | 'dot-dash' | 'disc' | 'ring' | 'line' | 'line-vertical' | 'weave' | 'zigzag' | 'zigzag-vertical' | 'diagonal' | 'diagonal-right-left' | 'square' | 'box' | 'triangle' | 'triangle-inverted' | 'diamond' | 'diamond-box',
     /** background color of pattern e.g. '#1f77b4' */
     backgroundColor: string,
     /** color of the pattern e.g. #000000 */
     patternColor?: string,
     size?: number): CanvasPattern;
-    export = draw;
-    export = generate;
+}
+
+declare module "patternomaly/generate" {
+    export function generate(colorList: Array<string>): Array<CanvasPattern>;
 }
